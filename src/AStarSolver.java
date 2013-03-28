0de64fb68fef592530bc45b8e1f60c401fd628d1
Jorge <sky_chainer@hotmail.com>
2013-03-27 20:49:34 -0600

    Create the skeleton for the AStarSolver

---
+++ src/AStarSolver.java
@@ -0,0 +1,22 @@
+
+public class AStarSolver extends Solver{
+
+	@Override
+	public boolean isSolvable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int moves() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterable<Board> solution() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
