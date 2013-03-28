7a32ad1e6cc8d0519149a42f68ed6292e0c799cf
Jorge <sky_chainer@hotmail.com>
2013-03-27 20:53:11 -0600

    Create the skeleton for the DFS Class

---
+++ src/DFSSolver.java
@@ -0,0 +1,22 @@
+
+public class DFSSolver extends Solver{
+
+	@Override
+	protected boolean isSolvable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected int moves() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected Iterable<Board> solution() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
