ef9cbad2544fb67058a6c3998a677adb8094c907
Jorge <sky_chainer@hotmail.com>
2013-04-06 14:07:46 -0600

    Change Board to Action methods

--- src/Solver.java
+++ src/Solver.java
@@ -1,13 +1,12 @@
-import java.util.Stack;
 
 public abstract class Solver {
 	protected boolean solvable;
 	protected int moves = -1;
-	protected Stack<Board> movements;
+	protected Stack<Action> movements;
 	
 	protected abstract boolean isSolvable();
     
     protected abstract int moves();
     
-    protected abstract Iterable<Board> solution();
+    protected abstract Iterable<Action> solution();
 }
